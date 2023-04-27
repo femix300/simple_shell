233d999ce271fa7a4e685554933293357bda1fa3
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-04-26 20:07:40 -0700

    updates B

--- num_args.c
+++ num_args.c
@@ -1,8 +1,14 @@
 #include "main.h"
+/**
+* get_num_args - a function that counts the njumber of arguments
+* @args: the arguments
+* Return: the number of arguments
+*/
 
 int get_num_args(char **args)
 {
 	int num_args;
+
 	num_args = 0;
 
 	while (args[num_args] != NULL)
