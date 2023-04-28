07642bfae742d4b9510b8c97c1e9588b4170b1df
Joseph Kimuchu <113996255+KimuchuJr@users.noreply.github.com>
2023-04-28 10:51:44 +0300

    Update execute_commands.c

--- execute_commands.c
+++ execute_commands.c
@@ -1,77 +1,41 @@
 #include "main.h"
-/**
- * handle_builtin_commands - A function that helps execute commands
- *
- * @args: Arguments
- *
- * Description: Command built-in access function
- *
- * Return: 0
- */
-int handle_builtin_commands(char **args)
-{
-	if (args[0] == NULL)
-		return (1);
-	if (my_strcmp(args[0], "exit") == 0)
-	{
-		int num_args = get_num_args(args);
 
-		kpsh_exit(args, num_args);
-		return (1);
-	}
-	else if (my_strcmp(args[0], "cd") == 0)
-	{
-		change_directory(args);
-		return (1);
-	}
-	else if (my_strcmp(args[0], "setenv") == 0)
-	{
-		set_env(args);
-		return (1);
-	}
-	else if (my_strcmp(args[0], "unsetenv") == 0)
-	{
-		unset_env(args);
-		return (1);
-	}
-	return (0);
-}
 /**
  * execute_command - execute shell commands
  *
- * @args: arguments to thr commands
+ * @args: arguments to the commands
  *
  * Description: This functions calls all built-in functions
  *
- * Return: 1 or 0
+ * Return: 1
  *
  */
 int execute_command(char **args)
 {
-	int status;
-	char *command = handle_path(args);
-	pid_t pid = fork();
-
-	if (handle_builtin_commands(args))
+	int stat, pid;
+	int flg = 0;
+	
+	args = my_chpath(args, &flg);
+	
+	if (args == NULL)
 		return (1);
-	if (access(command, X_OK) != 0)
-	{
-		fprintf(stderr, "%s: %d: %s: not found\n", "./hsh", 1, args[0]);
-		return (1);
-	}
-
+	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(command, args, environ) < 0)
+		if (execve(args[0], args, environ) == -1)
 		{
-			perror("Error: ");
-			exit(EXIT_FAILURE);
+			perror("hsh");
 		}
+		if (flg == 1)
+		free(args[0];
+		return (-1);
 	}
 	else if (pid < 0)
 	{
-		perror("Error:");
-		exit(EXIT_FAILURE);
+		perror("hsh:");
+		if (flg == 1)
+		free(args[0]);
+		return (-1);
 	}
 	else
 	{
@@ -79,6 +43,9 @@ int execute_command(char **args)
 			waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
+	if (flg == 1)
+	free(args[0]);
+	
 
 	return (1);
 }
